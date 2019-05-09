16bd8c1e23d620f06526e0e27d905cd7552ccc97
slore <slorelee@gmail.com>
2019-05-09 13:33:07 +0900

    add getopt() for compiling on Windows

--- binmay_win.c
+++ binmay_win.c
@@ -29,7 +29,7 @@ Changes:
 }}} */
 // {{{ include
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -100,6 +100,64 @@ int masked_string_isset(struct masked_string *masked);
 struct buffered_handle *buffered_handle_new(char *filename, FILE *def, const char *mode);
 struct file_handle *file_handle_new(char *filename, FILE *def, const char *mode);
 // }}}
+
+/* getopt function() from LCC for Windows */
+/******************************************/
+#include  <stdio.h>
+#include  <string.h>
+
+#define ERR(str, chr)       if(opterr){fprintf(stderr, "%s%c\n", str, chr);}
+
+int     opterr = 1;
+int     optind = 1;
+int optopt;
+char *optarg;
+
+int
+getopt(int argc, char *const argv[], const char *opts)
+{
+    static int sp = 1;
+    int c;
+    const char *cp;
+
+    if (sp == 1) {
+        if (optind >= argc ||
+            argv[optind][0] != '-' || argv[optind][1] == '\0')
+            return -1;
+        else if (strcmp(argv[optind], "--") == 0) {
+            optind++;
+            return -1;
+        }
+    }
+    optopt = c = argv[optind][sp];
+    if (c == ':' || (cp = strchr(opts, c)) == 0) {
+        ERR(": illegal option -- ", c);
+        if (argv[optind][++sp] == '\0') {
+            optind++;
+            sp = 1;
+        }
+        return c; //'?';
+    }
+    if (*++cp == ':') {
+        if (argv[optind][sp + 1] != '\0')
+            optarg = &argv[optind++][sp + 1];
+        else if (++optind >= argc) {
+            ERR(": option requires an argument -- ", c);
+            sp = 1;
+            return c; //'?';
+        } else
+            optarg = argv[optind++];
+        sp = 1;
+    } else {
+        if (argv[optind][++sp] == '\0') {
+            sp = 1;
+            optind++;
+        }
+        optarg = 0;
+    }
+    return c;
+}
+/******************************************/
 
 int main(int argc, char **argv) // {{{
 {
